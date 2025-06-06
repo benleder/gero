7cf355691d112b3bf0975b8749b224447997821a
Ben Leder <34696955+benleder@users.noreply.github.com>
2025-06-05 21:22:43 -0400

    Implement A* pathfinding for unit movement

--- src/models.rs
+++ src/models.rs
@@ -48,7 +48,7 @@ pub enum Faction {
     Chaos,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
